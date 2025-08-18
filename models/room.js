ea2c310965b375ed6ef992aa776492ae98dce206
yolcubalsude <sude.yolcubal@gmail.com>
2025-08-18 11:54:49 +0300

    Enhance Room model by adding roomId and createdAt fields

--- models/room.js
+++ models/room.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
 const RoomSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  password: { type: String, required: true } 
+  roomId: {
+    type: String,
+    required: true,
+    unique: true, // Aynı oda ID tekrar olmasın
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Room", RoomSchema);
